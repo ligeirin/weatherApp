941fe737e08b359e8f8e3d415fae03897df83e5b
Diogo Alves <diogobis23@gmail.com>
2024-05-17 21:42:16 -0300

    Update styling and weather API

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -126,5 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "weather/static/"
-    "/var/www/static/",
 ]
--- weather/views.py
+++ weather/views.py
@@ -7,25 +7,33 @@ from .forms import ConfigureForm
 
 import requests
 import json
+import re
 
 def index(request):
-    localeList = UserLocale.objects.order_by('country')
+    locale = UserLocale.objects.order_by('-pk').first()
+    
+    result = requests.get('http://api.weatherapi.com/v1/current.json?key=a5152a105dc7444ea43163315241705&q=' + locale.country + ',' + locale.federation + ',' + locale.city + '&aqi=no')
+    r_status = result.status_code
 
-    weather = []
-    for locale in localeList:
-        result = requests.get('http://api.weatherapi.com/v1/current.json?key=a5152a105dc7444ea43163315241705&q=' + locale.country + ',' + locale.federation + ',' + locale.city + '&aqi=no')
-        r_status = result.status_code
+    if r_status == 200:
+        data = result.json()
+        data['location']['formattedHour'] = re.search('\d{2}:\d{2}',data['location']['localtime']).group(0)
+        data['location']['currentHour'] = int(data['location']['formattedHour'].split(':')[0])
+        data['location']['timeOfDay'] = 'day'
 
-        if r_status == 200:
-            data = result.json()
-            locale.weather = data
-            # localeWeather = dict(locale)
-            # localeWeather['weather'] = data
-            # weather.append(localeWeather)
+        if data['location']['currentHour'] >= 19 or data['location']['currentHour'] < 5:
+            data['location']['timeOfDay'] = 'night'
+        elif data['location']['currentHour'] >= 5 and data['location']['currentHour'] <= 7:
+            data['location']['timeOfDay'] = 'golden-hour'
+        elif data['location']['currentHour'] >= 17 and data['location']['currentHour'] < 19:
+            data['location']['timeOfDay'] = 'golden-hour'
+        else:
+            data['location']['timeOfDay'] = 'day'
+
+        locale.weather = data
 
     context = {
-        "locales": localeList,
-        "weather": weather
+        "locale": locale,
     }
 
     return render(request, "weather/index.html", context)
